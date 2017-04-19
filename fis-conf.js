fc9f67eedab688efb4cbd7a0b0b637a2039fae58
chenjacky131 <chenjacky131@gmail.com>
2017-04-19 15:06:49 +0800

    Add files via upload

---
+++ fis-conf.js
@@ -0,0 +1,51 @@
+// 合并引入的css，js
+/*fis.match('::packager', {
+  postpackager: fis.plugin('loader',{// 合并页面里的零碎资源换成一个文件
+  	allInOne:{
+      js: function (file) {
+        //return "/static/js/" + file.filename + "_mi.js";
+         return "/static/js/mi.js";//自定义文件的命名方式
+      },
+      css: function (file) {
+       // return "/static/css/" + file.filename + "_mi.css";
+       return "/static/css/mi.css";//自定义文件的命名方式
+      }  		
+  	}
+  })
+});*/
+// 修改静态资源目录
+/*fis.match('*.{png,js,css,jpg}', {
+  release: '/static/$0'
+});*/
+// less的编译
+fis.match('*.less',{
+	parser:fis.plugin('less'),
+	rExt:'.css'
+});
+// scss的编译
+fis.match('*.scss',{
+	parser:fis.plugin('node-sass'),
+	rExt:'.css'
+});
+// autoprefixer 自动添加css浏览器前缀
+fis.match('*.{css,less,scss}', {
+  preprocessor: fis.plugin('autoprefixer', {
+    "browsers": ["Android >= 2.1", "iOS >= 4", "ie >= 8", "firefox >= 15"],
+    "cascade": true
+  })
+});
+// 压缩js文件
+fis.match('*.{js,jsx,ts,tsx,es6,es}', {
+  optimizer: fis.plugin('uglify-js')
+});
+// 压缩css文件
+fis.match('*.{scss,sass,less,css}', {
+  optimizer: fis.plugin('clean-css')
+});
+// png图片压缩
+fis.match('*.png', {
+  optimizer: fis.plugin('png-compressor',{
+  	 type : 'pngquant'
+  })
+});
+
